fb75f552db9d3ca262eb0220039c16c63d24b6c3
Alejandro Desatnik <adesatnik@uchicago.edu>
2016-01-14 16:33:08 -0600

    renamed files

--- addcrosslistings.py
+++ addcrosslistings.py
@@ -1,17 +1,9 @@
 __author__ = 'Alejandro'
-import urllib
-import urllib2
-import requests
+from django.core.management.base import BaseCommand, CommandError
 import cookielib
+import requests
 from bs4 import BeautifulSoup
 from planner.models import Quarter, Course
-from django.core.management.base import BaseCommand, CommandError
-
-
-
-
-
-
 
 def get_quarter(url,quarter):
     URL = url
@@ -33,78 +25,53 @@ def get_quarter(url,quarter):
     return s.post(URL, data = {"TermName" : quarter}).text
 
 
-def parse_department_page(url, q):
-
-    html = get_quarter(url, q.quarter + " " + str(q.year))
+def scrapecrosslistings(course):
+    lastqoffered = course.quarter_set.extra(order_by = ["-index"])[0]
+    html = get_quarter("https://classes.uchicago.edu/courseDetail.php?courseName=" + str(course), str(lastqoffered))
     soup = BeautifulSoup(html, "lxml")
-    classes = []
+    try:
+        crossp = soup.find(id="tabs-1").find("p")
+    except:
+        crossp = ""
+
+    if crossp:
+        for l in crossp.find_all("a"):
+            coursename = l.string.split(" ")
+
+            try:
+                lastcourseversion = Course.objects.filter(department=coursename[0], code=coursename[1])[0]
+            except:
+                c = Course(department=coursename[0], code=coursename[1], name=course.name)
+                c.save()
+                c.quarter_set.add(lastqoffered)
+                print "added " + str(c)
+                lastcourseversion = c
+            course.cross_listings.add(lastcourseversion)
 
 
-    rows =soup(class_="resultrow")
-
-    for row in rows:
-        classes.append([
-            row.find(class_="name").a.next_sibling.strip(),
-            row.find(class_="two").string.strip()
-        ])
+    else:
+        return None
 
 
 
 
 
-    for cl in classes:
-        if not Course.objects.filter(name=cl[1], department=(cl[0].split(" "))[0],
-                   code=(((cl[0].split(" "))[1]).split("/"))[0]):
-            c = Course(name=cl[1], department=(cl[0].split(" "))[0],
-                       code=(((cl[0].split(" "))[1]).split("/"))[0]  )
-            c.save()
 
 
-            c.quarter_set.add(q)
 
-        else:
-            c = Course.objects.get(name=cl[1], department=(cl[0].split(" "))[0],
-                   code=(((cl[0].split(" "))[1]).split("/"))[0])
 
-            c.quarter_set.add(q)
+
+
 
 
 class Command(BaseCommand):
-    help = "Scrapes classes.uchicago for classes on the given quarter"
+    help = "Scrapes cross-listings out of every class in the database"
 
-    def add_arguments(self, parser):
-        parser.add_argument("quarter", nargs="+" ,type=str)
-        parser.add_argument("year", nargs="+", type=int)
 
     def handle(self, *args, **options):
+        for c in Course.objects.filter(id__gt=2954):
+            scrapecrosslistings(c)
 
-            linkstrings = []
-
-            html = get_quarter("https://classes.uchicago.edu/browse.php",
-                               str(options["quarter"][0]) + " " + str(options["year"][0]))
-
-            soup = BeautifulSoup(html, "lxml")
-            if Quarter.objects.filter(quarter = options["quarter"][0], year = int(options["year"][0])):
-                q = Quarter.objects.get(quarter = options["quarter"][0], year = int(options["year"][0]))
-            else:
-                q = Quarter(quarter = options["quarter"][0], year = int(options["year"][0]))
-                q.save()
-
-            filtered = soup.find(id="tabs-1")
-            filtered = filtered("li")
-
-            for link in filtered:
-                linkstrings.append("https://classes.uchicago.edu/" + link.find("a").get("href"))
-
-
-
-
-            for link in linkstrings:
-                parse_department_page(link,q)
-                print(link + " done")
-
-
-
-
-
+            print c.cross_listings.all()
+            print c.id
 
